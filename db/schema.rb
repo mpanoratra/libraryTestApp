669441a0fc2f9836b2e32133ff5e642988e90ed6
stv8 <stvillars8@gmail.com>
2014-07-16 20:55:09 -0500

    add quantity to books

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140615191348) do
+ActiveRecord::Schema.define(version: 20140717003107) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 20140615191348) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "author_id"
+    t.integer  "quantity"
   end
 
   create_table "genres", force: true do |t|
