f1bafb93a13b2f66bf9e3d46b157632a823e7113
Brandon Gottesman <brandong@perfectlynot.com>
2014-07-20 15:10:28 -0500

    db changes

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140717003107) do
+ActiveRecord::Schema.define(version: 20140720194538) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20140717003107) do
     t.datetime "updated_at"
     t.integer  "author_id"
     t.integer  "quantity"
+    t.integer  "checkout_ct"
   end
 
   create_table "genres", force: true do |t|
