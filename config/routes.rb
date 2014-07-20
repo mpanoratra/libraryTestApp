2400de6566a92561b1cba6944fb04b7a76468814
stv8 <stvillars8@gmail.com>
2014-07-20 15:06:52 -0500

    checkout book

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   resources :genres
 
   resources :books
-
+  get 'books/:id/checkout' => 'books#checkout'
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
